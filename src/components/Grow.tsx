fbd74ed63d343893cbc2dffefa6228e80c2074d4
tseijp <t1810394@edu.cc.uec.ac.jp>
2021-01-11 17:55:52 +0900

    :wrench: add Grow

--- src/components/Grow.tsx
+++ src/components/Grow.tsx
@@ -1,27 +1,43 @@
-import React, {useEffect, useRef} from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import {animated} from 'react-spring'
+import {animated, useSpring} from 'react-spring'
 import {Props} from '../types'
 
 const Item = styled<any>(animated.div)`
-    position: relative;
-    display: grid;
-    margin: ${props => props.size*50}px auto 0 auto;
-    width: ${props => props.size*250}px;
-    height: ${props => props.size*250}px;
+    top: 50%;
+    left: 50%;
+    margin: 0px;
+    transform: translateX(-50%) translateY(-50%);
+    border: 1rem solid ${props => props.color};
+    background: ${props => props.color};
+    border-radius: 100%;
+    position: absolute;
 `
 
-export type Grow = {
-    (props: Props): JSX.Element
-}
+export type Grow = {(props: Props): JSX.Element}
 export const Grow = React.forwardRef(({
-    onView=null,
-    size=1, className="spinner-grow"//, ...props
-}: any, forwardRef) => {
-    const fn = useRef()
-    const ref = useRef(null)
-    React.useImperativeHandle(forwardRef, () => ref.current)
-    useEffect(()=>{fn.current = onView}, [onView])
-    return <Item {...{ref,size,className}} />
-}
-)
+    active=true,
+    dark=1,
+    size=1,
+    width=250,
+    timeout=250,
+}: any, ref) => {
+    const [close, set] = React.useState(false)
+    const {opacity, r} = useSpring({
+        reset: true,
+        from: { opacity: 1, r: 0},
+        to: (active && !close)
+            ? async next => {
+                await next({ opacity: 0, r: width * size})
+                set(true)
+                setTimeout(() => void set(false), timeout)
+            }
+            : { opacity: 1, r: 0}
+    })
+    return close? null:  (
+        <Item style={{opacity, width: r, height: r}}
+            color={dark? "#818181": "#212121"}
+            ref={ref}
+            size={size}/>
+    )
+})
